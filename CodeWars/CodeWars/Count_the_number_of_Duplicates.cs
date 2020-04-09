e4b12e05a23617321f9d4e9c9f6df347179ee3e0
Przemysław Całka <przemekcalka1@outlook.com>
2020-04-09 19:41:06 +0200

    Count numbers of duplicates

---
+++ CodeWars/CodeWars/Count_the_number_of_Duplicates.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Count_the_number_of_Duplicates
+    {
+        public int DuplicateCount(string str)
+        {
+            var chars = new Dictionary<char, int>();
+            var result = str.ToLower().Select(c => {
+                if (chars.ContainsKey(c) && chars[c] == 1)
+                {
+                    chars[c] = 2;
+                    return 1;
+                }
+                if (!chars.ContainsKey(c))
+                {
+                    chars[c] = 1;
+                }
+                return 0;
+            }).Sum();
+
+            return result;
+        }
+
+        public int DuplicateCount2(string str)
+        {
+            return str.ToLower().GroupBy(c => c).Where(c => c.Count() > 1).Count();
+        }
+    }
+}
