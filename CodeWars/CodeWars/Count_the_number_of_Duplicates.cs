21c9b25c8d48dd171f0afa44f7f782c4b89c042f
Przemysław Całka <przemekcalka1@outlook.com>
2020-04-14 20:23:54 +0200

    Persistent Bugger

--- CodeWars/CodeWars/Count_the_number_of_Duplicates.cs
+++ CodeWars/CodeWars/Count_the_number_of_Duplicates.cs
@@ -5,6 +5,18 @@ namespace CodeWars
 {
     class Count_the_number_of_Duplicates
     {
+//Count the number of Duplicates
+//Write a function that will return the count of distinct case-insensitive alphabetic 
+//characters and numeric digits that occur more than once in the input string. 
+//The input string can be assumed to contain only alphabets(both uppercase 
+//and lowercase) and numeric digits.
+//Example
+//"abcde" -> 0 # no characters repeats more than once
+//"aabbcde" -> 2 # 'a' and 'b'
+//"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+//"indivisibility" -> 1 # 'i' occurs six times
+//"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+
         public int DuplicateCount(string str)
         {
             var chars = new Dictionary<char, int>();
---
+++ CodeWars/CodeWars/Persistent_Bugger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Persistent_Bugger
+    {
+        //Write a function, persistence, that takes in a positive parameter num and returns
+        //its multiplicative persistence, which is the number of times you must multiply 
+        //the digits in num until you reach a single digit.
+        //For example:
+        //persistence(39) == 3  because 3*9 = 27, 2*7 = 14, 1*4=4
+        //and 4 has only one digit
+        public static int Persistence(long n)
+        {
+            int i = 0;
+            while (n > 9l)
+            {
+                long mul = 1l;
+
+                while (n > 0l)
+                {
+                    mul *= n % 10l;
+                    n /= 10l;
+                }
+
+                n = mul;
+                i++;
+            }
+            return i;
+        }
+    }
+}
