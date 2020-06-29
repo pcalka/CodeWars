181fa60208a0b8c5f996ed4759bc03c02bcff589
Przemysław Całka <przemekcalka1@outlook.com>
2020-06-29 20:49:26 +0200

    Sort odd.

---
+++ CodeWars/CodeWars/SortOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class SortOdd
+    {
+        public  int[] SortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] % 2 == 1 && array[j] % 2 == 1)
+                        if (array[i] > array[j])
+                        {
+                            int temp = array[j];
+                            array[j] = array[i];
+                            array[i] = temp;
+                        }
+                }
+            }
+            return array;
+        }
+    }
+}
